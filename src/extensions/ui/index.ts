cf052142570a9d15d46eb926fda5a8d9b5de3683
thien2218 <thien1367526@gmail.com>
2026-05-09 05:18:04 +0000

    update branding and commands

--- src/extensions/ui/index.ts
+++ src/extensions/ui/index.ts
@@ -1,6 +1,8 @@
 import type { ExtensionAPI } from "@earendil-works/pi-coding-agent";
+import brandingExtension from "./branding.js";
 import inputModeIndicatorExtension from "./input-mode-indicator.js";
 
 export default async function uiExtensions(pi: ExtensionAPI) {
+  await brandingExtension(pi);
   await inputModeIndicatorExtension(pi);
 }

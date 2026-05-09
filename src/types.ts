7ad400aa2fbce85db0b93dd73e192ef204b0f3c7
Thien Huynh <thien.huynh@opswat.com>
2026-05-09 13:25:57 +0000

    refactor web tools

--- src/types.ts
+++
@@ -1,4 +0,0 @@
-import { WEB_TOOLS_PROVIDERS } from "./settings.js";
-
-export type WebToolsProvider = (typeof WEB_TOOLS_PROVIDERS)[number];
-export type WebToolsProviderId = WebToolsProvider["id"];

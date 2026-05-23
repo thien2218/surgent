464c59aa01038b9abc6b5e36799173e385be2ec5
thien2218 <thien1367526@gmail.com>
2026-05-23 04:53:11 +0000

    remove scroll to bottom as it is not feasible

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -1,10 +1,8 @@
 import type { ExtensionAPI } from "@earendil-works/pi-coding-agent";
 import brandingExtension from "./branding.js";
 import inputModeIndicatorExtension from "./input-mode-indicator.js";
-import scrollToBottomExtension from "./scroll-to-bottom.js";
 
 export default function uiExtensions(pi: ExtensionAPI) {
   brandingExtension(pi);
   inputModeIndicatorExtension(pi);
-  scrollToBottomExtension(pi);
 }

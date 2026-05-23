aeecf9cb923569132d3563b48d7befbf394a3145
thien2218 <thien1367526@gmail.com>
2026-05-23 03:27:47 +0000

    add surgent development guideline and scroll to bottom feature

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -1,8 +1,10 @@
 import type { ExtensionAPI } from "@earendil-works/pi-coding-agent";
 import brandingExtension from "./branding.js";
 import inputModeIndicatorExtension from "./input-mode-indicator.js";
+import scrollToBottomExtension from "./scroll-to-bottom.js";
 
 export default function uiExtensions(pi: ExtensionAPI) {
   brandingExtension(pi);
   inputModeIndicatorExtension(pi);
+  scrollToBottomExtension(pi);
 }

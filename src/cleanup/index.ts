1f388fe0e5210599cb2c7fae8b0d5f849624a39f
thien2218 <thien1367526@gmail.com>
2026-06-18 04:54:00 +0000

    enhance: added background jobs for cleaning up deleted sessions data and git stashes

--- src/cleanup/index.ts
+++ src/cleanup/index.ts
@@ -1,8 +1,17 @@
-import type { ExtensionAPI } from "@earendil-works/pi-coding-agent";
+import { SessionManager, type ExtensionAPI } from "@earendil-works/pi-coding-agent";
 import { cleanupPermissions } from "./permission.js";
+import { cleanupCheckpointStashes } from "./stash.js";
+import { pruneSessionFile } from "./helpers.js";
+import { getPiPath } from "../utils.js";
 
 export default function (pi: ExtensionAPI) {
-  pi.on("session_start", (_event, ctx) => {
-    cleanupPermissions(ctx.cwd);
+  pi.on("session_start", async (_event, ctx) => {
+    const sessions = await SessionManager.list(ctx.cwd);
+    const sessionIds = new Set(sessions.map((session) => session.id));
+
+    cleanupPermissions(ctx.cwd, sessionIds).catch(() => undefined);
+    pruneSessionFile(getPiPath("checkpoints", ctx.cwd), sessionIds).catch(() => undefined);
+    pruneSessionFile(getPiPath("sessionAgents", ctx.cwd), sessionIds).catch(() => undefined);
+    cleanupCheckpointStashes(pi, ctx.cwd, sessionIds).catch(() => undefined);
   });
 }

315bd073be2492144943f4573b087fe13968ed60
thien2218 <thien1367526@gmail.com>
2026-07-07 13:34:06 +0000

    feat: added support for java, python and go

---
+++ src/optimizers/languages/tree-sitter-languages.d.ts
@@ -0,0 +1,17 @@
+declare module "tree-sitter-go" {
+  import type { Language } from "tree-sitter";
+  const language: Language;
+  export default language;
+}
+
+declare module "tree-sitter-java" {
+  import type { Language } from "tree-sitter";
+  const language: Language;
+  export default language;
+}
+
+declare module "tree-sitter-python" {
+  import type { Language } from "tree-sitter";
+  const language: Language;
+  export default language;
+}
